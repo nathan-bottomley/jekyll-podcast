bacd3b9bab0b38ef5b7372c2e6557582056741ac
Nathan Bottomley <nathan.bottomley@gmail.com>
2021-09-30 07:11:54 +1000

    Added tag pages

---
+++ lib/jekyll/podcast/tag_link_filter.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'jekyll/podcast/tag_page_generator'
+
+module Jekyll
+  module Podcast
+    # creates a filter to turn tag names into links
+    module TagLinkFilter
+      def tag_link(input)
+        tag_href = Jekyll::Podcast::TagPageGenerator.permalink(input, @context.registers[:site])
+        "<a class='tag' href='/#{tag_href}'>#{input}</a>"
+      end
+    end
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::Podcast::TagLinkFilter)
---
+++ lib/jekyll/podcast/tag_page_generator.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+require 'jekyll/utils'
+
+module Jekyll
+  module Podcast
+    # Generator to create tag pages for each tag on the site
+    module TagPageGenerator
+      # The generator itself
+      class Generator < Jekyll::Generator
+        def generate(site)
+          site.tags.each do |tag, _|
+            site.pages << TagPage.new(site, tag)
+          end
+        end
+      end
+
+      # Represents a tag page; includes pagination information
+      class TagPage < Jekyll::Page
+        def initialize(site, tag)
+          super(site, site.baseurl, '/', 'index.html')
+          @tag = tag
+          @data ||= {}
+          @data['permalink'] = Jekyll::Podcast::TagPageGenerator.permalink(@tag, site)
+          @data['layout'] = 'tag-page'
+          @data['title'] = tag
+          @data['pagination'] = pagination
+        end
+
+        def pagination
+          {
+            'enabled' => true,
+            'tag' => @tag,
+            'sort_reverse' => false
+          }
+        end
+      end
+
+      def self.permalink(tag, site)
+        if site.data['tag_permalinks'] && site.data['tag_permalinks'][tag]
+          site.data['tag_permalinks'][tag]
+        else
+          Jekyll::Utils.slugify(tag)
+        end
+      end
+    end
+  end
+end
