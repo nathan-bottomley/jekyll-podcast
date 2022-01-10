69fef628afa00eb2fcb75102b09fd113f9980ba4
Nathan Bottomley <nathan.bottomley@gmail.com>
2022-01-10 18:42:25 +1100

    Allowed feed to use an include  for description

--- lib/jekyll/podcast.rb
+++ lib/jekyll/podcast.rb
@@ -11,6 +11,7 @@ end
 require 'jekyll/podcast/episode_data'
 require 'jekyll/podcast/podcast_episode_drop'
 require 'jekyll/podcast/liquid_tag_filters'
+require 'jekyll/podcast/file_exists'
 require 'jekyll/podcast/feed_generator'
 require 'jekyll/podcast/tag_page_generator'
 require 'jekyll/podcast/tag_link_filter'
---
+++ lib/jekyll/podcast/file_exists.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module Jekyll
+  module Podcast
+    # Liquid tage to determine the existence of a file
+    # Path name should be the root of the directory, without an initial slash
+    class FileExistsTag < Liquid::Tag
+      def initialize(tag_name, path, tokens)
+        super
+        @path = path
+      end
+
+      def render(context)
+        # Pipe parameter through Liquid to make additional replacements possible
+        url = Liquid::Template.parse(@path).render context
+
+        # Add the site source, so that it also works with a custom one
+        site_source = context.registers[:site].config['source']
+        file_path = "#{site_source}/#{url}"
+
+        # Check if file exists (returns true or false)
+        File.exist?(file_path.strip!).to_s
+      end
+    end
+  end
+end
+
+Liquid::Template.register_tag('file_exists', Jekyll::FileExistsTag)
