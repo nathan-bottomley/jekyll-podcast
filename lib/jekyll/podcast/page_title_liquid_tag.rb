58c5a29ae0ac79120518e3d0e5c82eb91568b063
Nathan Bottomley <nathan.bottomley@gmail.com>
2022-12-15 08:47:21 +1100

    Added pagetitletext liquid tag

--- lib/jekyll/podcast/page_title_liquid_tag.rb
+++ lib/jekyll/podcast/page_title_liquid_tag.rb
@@ -11,7 +11,22 @@ module Jekyll
         if page_title.nil? || page_title.empty? || page_title == site_title
           "<title>#{site_title}</title>"
         else
-          "<title>#{page_title} — #{site_title}</title>"
+          "<title>#{page_title} &middot; #{site_title}</title>"
+        end
+      end
+    end
+
+    # Liquid tag for generating the text of a page title 
+    # without the <title> tag
+    class PageTitleTextTag < Liquid::Tag
+      def render(context)
+        site_title = context.registers[:site].config['title']
+        page_title = context.registers[:page]['title']
+
+        if page_title.nil? || page_title.empty? || page_title == site_title
+          site_title
+        else
+          "#{page_title} &middot; #{site_title}"
         end
       end
     end
@@ -19,3 +34,4 @@ module Jekyll
 end
 
 Liquid::Template.register_tag('pagetitle', Jekyll::Podcast::PageTitleTag)
+Liquid::Template.register_tag('pagetitletext', Jekyll::Podcast::PageTitleTextTag)
